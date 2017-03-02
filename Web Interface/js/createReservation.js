4f565456a1705412da5cf4ab96981f000d12f455
uknomecarlos <castro_carlos6@hotmail.com>
2017-03-02 14:45:12 -0500

    comments for clarification

--- Web Interface/js/createReservation.js
+++ Web Interface/js/createReservation.js
@@ -1,7 +1,8 @@
-// Get a reference to the database service
+// Get references to the database service and Reservations
 var ref = firebase.database().ref();
 var resRef = ref.child('Reservations');
 
+// Checks that all of the fields have a value
 function validateForm(name, number, date, time) {
 	
     if (name== "") {
@@ -30,6 +31,9 @@ function validateForm(name, number, date, time) {
 
 }
 
+// Will run when the confirm reservation button is clicked,
+// Pulls values from the form and asks if information is correct,
+// if so will check if all fields have values, pushes form inputs to database
 function confirmReservation(){
 	
 	var name = document.forms["createReservation"]["partyName"].value;
@@ -40,17 +44,18 @@ function confirmReservation(){
 	if(confirm('Is the information correct?') && validateForm(name, number, date, time)){
 		window.location.href = "manageReservations.html";
 		createReservation(name, number, date, time);
-		return true;
 	}
 	else{
+		// should not clear fields if the information is not correct
 		return;
 	}
 }
-
+// if "Return to Reservations" is clicked, alert will pop up,
+// cancel should keep form data and stay on page
 function loseInformation(){
-	if(name.length > 0 || number > 0){
+	if(name.length > 0 || number.length > 0){
 		if(confirm('Are you sure? You will lose all of your data')){
-			
+			window.location.href = "manageReservations.html";
 		}
 		else{
 			return;
@@ -59,9 +64,9 @@ function loseInformation(){
 	}
 }
 
-// FireBase calls
-
+// Pushes data from form fields to firebase database in Reservation directory
 function createReservation(name, number, date, time){
+	//standard reservation children
 	var reservation = {partyName: name, partyNumber: number, resDate: date, resTime: time};
 	
 	resRef.push(reservation);	
