a62c4b37494339421c35b6d3db070cc4df0e6ce2
uknomecarlos <castro_carlos6@hotmail.com>
2017-04-02 21:38:10 -0400

    added create Reservation page

--- Web Interface/js/createReservation.js
+++ Web Interface/js/createReservation.js
@@ -5,16 +5,16 @@ var resRef = ref.child('Reservations');
 // Checks that all of the fields have a value
 function validateForm(name, number, date, time) {
 	
-    if (name== "") {
+    if (name === "") {
         alert("Party name must be filled out");
         return false;
     }
     else if (number == "") {
-        alert("Number in Party must be filled out");
+        alert("Party Size must be filled out");
         return false;
     }
 	else if(number < 1){
-		alert("Need at least 1 in prty");
+		alert("Need at least 1 in party");
 		return false;
 	}
     else if (date == "") {
@@ -40,10 +40,11 @@ function confirmReservation(){
 	var	number = document.forms["createReservation"]["partyNumber"].value;
 	var	date = document.forms["createReservation"]["date"].value;
 	var	time = document.forms["createReservation"]["time"].value;
-	
+    var dateInt = createDateTime(date, time);
+    
 	if(confirm('Is the information correct?') && validateForm(name, number, date, time)){
 		window.location.href = "manageReservations.html";
-		createReservation(name, number, date, time);
+		createReservation(name, number, dateInt);
 	}
 	else{
 		// should not clear fields if the information is not correct
@@ -64,10 +65,35 @@ function loseInformation(){
 	}
 }
 
+function createDateTime(date, time) {
+    var year = date.substring(0,4);
+    var month = date.substring(5,7);
+    var day = date.substring(8,10);
+    
+    var hours = time.substring(0,2);
+    var minutes = time.substring(3,5);
+    
+    var dateTime = new Date(year, month, day, hours, minutes);
+    
+    
+    
+    var dateInt = dateTime.getTime();
+    
+    return dateInt;
+}
+
 // Pushes data from form fields to firebase database in Reservation directory
-function createReservation(name, number, date, time){
+function createReservation(name, number, dateInt){
 	//standard reservation children
-	var reservation = {partyName: name, partyNumber: number, resDate: date, resTime: time};
+    
+    window.console.log(dateInt);
+
+	var reservation = {name: name, partySize: number, dateTime: dateInt};
+    
+    var currentTime = new Date();
 	
-	resRef.push(reservation);	
+    //resNumber = resRef.child('resCounter').key;
+    resNumber = currentTime.getTime(); // change to resCounter when you figure it out
+    //resRef.child('resCounter').update( 1);
+    ref.child('Reservations/' + resNumber).set(reservation);	
 }
---
+++ Web Interface/js/editAccount.js
@@ -0,0 +1,11 @@
+function addToDropdown() {
+    <a class="dropdown-item"> Carlos2 </a>
+    /*
+    var options = '';
+    $.each(data, function(index, value) {
+        options += '<option value="' + car + '" text="' + car + '" />';
+    });
+
+    $('.accountDropdown').append(options);
+    */
+}
