6bae924aea8a2f66053a64fa059225ff52d766b5
McNamaraB1 <bjmcnamara@earthlink.net>
2017-04-06 20:55:19 -0400

    Add files via upload

--- Web Interface/js/createMenuItem.js
+++ Web Interface/js/createMenuItem.js
@@ -1,15 +1,31 @@
 // Get references to the database service and Menu
 var menus = firebase.database().ref().child('Menu');
-var menuRef = menus.child('MenuItems');
+var menuItems = menus.child('MenuItems');
+
+function makeMenuItem(){
+    var itemName = document.forms["createMenuItem"]["itemName"].value;
+	var itemPrice = document.forms["createMenuItem"]["itemPrice"].value;
+    
+    if(validateForm(itemName, itemPrice) === true){
+        
+        console.log(itemName + " " + itemPrice)
+        
+        createMenuItem(itemName, itemPrice);
+        
+        //ensure reloading from server instead of cache
+        location.reload(true);
+
+    }
+}
 
 // Checks that all of the fields have a value
-function validateForm(name, price) {
+function validateForm(itemName, itemPrice) {
 	
-    if (name === "") {
+    if (itemName === "") {
         alert("Name must be filled out");
         return false;
     }
-    else if (price == "") {
+    else if (itemPrice == "") {
         alert("Price must be filled out");
         return false;
     }
@@ -18,45 +34,15 @@ function validateForm(name, price) {
 	}
 
 }
-
-// Will run when the confirm create Menu Item button is clicked,
-// Pulls values from the form and asks if information is correct,
-// if so will check if all fields have values, pushes form inputs to database
-function confirmMenuItem(){
-	
-	var name = document.forms["createMenuItem"]["itemName"].value;
-	var	price = document.forms["createMenuItem"]["itemPrice"].value;
+       
+// Pushes data from form fields to firebase database in Reservation directory
+function createMenuItem(itemName, itemPrice){
+	//standard reservation children
     
-	if(validateForm(name, price)){
-		window.location.href = "menu.html";
-		createMenuItem(name, price);
-	}
-	else{
-		// should not clear fields if the information is not correct
-		return;
-	}
-}
-
-// Pushes data from form fields to firebase database in Menu directory
-function createMenuItem(itemName, ItemPrice){
-	//standard menu children
-	var newMenuItem = {name: itemName, price: itemPrice};
+    var newMenuItem = {name: itemName, price: itemPrice};
 	
-	var mName = itemName;
+    menus.child('MenuItems/' + itemName).set(newMenuItem);	
 	
-    menus.child('MenuItems/' + mName).set(newMenuItem);	
-}
-
-// if "Return to Menu" is clicked, alert will pop up,
-// cancel should keep form data and stay on page
-function loseInformation(){
-	if(name.length > 0 || price.length > 0){
-		if(confirm('Are you sure? You will lose all of your data')){
-			window.location.href = "menu.html";
-		}
-		else{
-			return;
-		}
-		
-	}
+	window.alert("Created " + itemName + ".");  
+    
 }
