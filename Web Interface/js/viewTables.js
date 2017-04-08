675839653b021670ac926b2f283913b8ee44cfd6
S. Brady Davis <stephen.b.davis@outlook.com>
2017-04-08 18:24:46 -0400

    Add files via upload

--- Web Interface/js/viewTables.js
+++ Web Interface/js/viewTables.js
@@ -1,85 +1,75 @@
 var tables = firebase.database().ref().child('Tables'); // references to firebase
+var tableList = tables.child('Tables');
 
 // Generate the tables and their status on page load.
 function loadTables(){
-    
-    // Create a snapshot for the table directory
-    tables.once("value")
-    .then(function(snapshot) {
-        
-        // Run through each table in the table list.
-        snapshot.forEach(function(childSnapshot) {
-            
-            var tableNum = childSnapshot.key;
-            var childData = new Array();
-            
-            // Cycle through each child of each table and childData will hold the values
-            // from the table status, owner, etc.
-            childSnapshot.forEach(function(orderValues){
-                childData.push(orderValues.val());
-            });
-            
-            // Ignore the "Examples" table (why not?)
-            if (tableNum == "Example"){
-                // Do nothing.   
-            }
-            
-            // Print the table
-            else {
-                printTables(tableNum, childData, "capacity");
-                printTables(tableNum, childData, "key");
-                printTables(tableNum, childData, "name");
-                printTables(tableNum, childData, "newStatus");
-                printTables(tableNum, childData, "reservationName");
-                printTables(tableNum, childData, "status");
-            }
-            
-        });
-    });
-    
 
-    // Add a new row to the table
-    function addToTable(tableNum, childData, tableStatus){
-        // Declare the edit and delete buttons
-        var editButton = '<a class="blue-text" id="'+tablNum+'" onClick="editAction(this.id)"><i class="fa fa-pencil"></i></a>';
-        var deleteButton = '<a class="red-text" id="'+tableNum+'" onClick="deleteAction(this.id)"><i class="fa fa-times">';
-        
-        // Create a row with 6 horizontal cells in tableTable in viewTables.html
-        var table = document.getElementById("tableTable");
-        var rowCount = table.rows.length; 
-        var row = table.insertRow(rowCount);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
-        var cell6 = row.insertCell(5);
-
-        // fill in each cell in the row
-        cell1.innerHTML = tableNum;  // Table Number
-        cell2.innerHTML = childData[3];// Name
-        cell3.innerHTML = childData[1]; // Capacity
-        cell4.innerHTML = childData[6]; //  should be table status
-        cell5.innerHTML = childData[5]; // Reservation Name.
-        cell6.innerHTML = editButton + " " + deleteButton; // self-explanatory
+  var query = firebase.database().ref('Tables').orderByChild("key");
+  query.once("value").then(function(snapshot) {
+  snapshot.forEach(function(childSnapshot) {
+    var table = childSnapshot.key;
+    var childData = childSnapshot.val();
+    if (table == 'tableCounter') {
+      ; // Do nothing
     }
-    
-    function printTables(tableNum, childData, tableStatus){
-        var currTableRef = tables.child(tableStatus); // reference to firebase for tableStatus
-
-        // snapshot for tableStatus
-        currTableRef.once("value")
-            .then(function(snapshot) {
-            // Cycle through each child of tableStatus
-                  snapshot.forEach(function(childSnapshot){
-                  // if the tableNumber is a child of tableStatus is will be added to the table with that table status.
-                    if (childSnapshot.val() == talbeNum){
-                        addtoTable(tableNum, childData, tableStatus)
-                    }
-                    else{
-                        //Nothing
-                    }
-                });
-        });
+    else {
+      addToTable(childData);
     }
+
+  });
+ });
+
+ function addToTable(data){
+
+   // Create edit and delete buttons
+   var editButton = '<a class="blue-text" id="'+data.name+'" onClick="editAction(this.id)"><i class="fa fa-pencil"></i></a>';
+   var deleteButton = '<a class="red-text" id="'+data.name+'" onClick="deleteReservation(this.id)"><i class="fa fa-times">';
+
+   // Create the table for viewTables.html
+   var table = document.getElementById('tableTable');
+   var rowCount = table.rows.length;
+   var row = table.insertRow(rowCount);
+   var cell1 = row.insertCell(0);
+   var cell2 = row.insertCell(1);
+   var cell3 = row.insertCell(2);
+   var cell4 = row.insertCell(3);
+   var cell5 = row.insertCell(4);
+
+   // Populate the table
+   cell1 = data.name;
+   cell2 = data.capacity;
+   cell3 = data.status;
+   cell4 = data.reservationName;
+   cell5 = editButton + " " + deleteButton;
+ }
+}
+
+function editAction(tableName){
+  // Save the table name to localStorage for retrieval in "editTable.html"
+  localStorage.setItem("tableName", tableName);
+  window.location.href = './editTable.html'; // Move to editTable.html
+}
+
+function deleteTable(tableName){
+  // Popup confirmation
+  if(confirm('Are you sure you wish to delete the table \"' + tableName + '\"?')){
+    var query = firebase.database().ref('Tables');
+    query.once("value")
+      .then(function(snapshot) {
+        // Search out the table to be removed.
+        snapshot.forEach(function(childSnapshot){
+          var table = childSnapshot.key;
+          var childData = childSnapshot.val();
+          var n = tableName.localeCompare(childData.name);
+          if (n == 0)
+            {
+              // Purge the table.
+              firebase.database().ref('Tables/' + table).remove();
+              location.reload(true);
+            }
+
+        });
+      });
+  }
+
 }
